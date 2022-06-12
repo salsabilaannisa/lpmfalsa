e5022c440ce4ccc2ba88a04a16c94ab587a4c1d3
salsabilaannisa <salsabilaannisa09@gmail.com>
2022-06-13 00:10:30 +0700

    categories controller

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -3,16 +3,26 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Post;
 
 class CategoriesController extends Controller
 {
     public function index()
     {
+        $kategori = Category::all();
+        foreach($kategori as $item){
+            $item->images = null;
+            $images = Post::where('category_id', $item->id)->where('status', 'published')->first();
+            if($images){
+                $item->images = $images->image;
+            }
+
+        }
         return view('categories', [
             'title' => 'Post Categories', 
             'active' => 'categories',
             "mycategory" => $this->MyCategory,
-            'categories' => Category::all()
+            'categories' => $kategori
         ]); 
     }
 }
